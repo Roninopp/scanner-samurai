b7f7efaece572c5dbaa6e88675b2bac03f9320ed
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-03-16 22:09:32 +0530

    Create strings.py

---
+++ samurai/strings.py
@@ -0,0 +1,10 @@
+scan_request_u = """
+$SCAN
+Cymatic Scan request!
+**CASE ID**: `{}`
+**Enforcer:** {} 
+**User scanned:** [{}](tg://openmessage?user_id={})
+**Reason:** {}
+**proof:** `{}`
+**bancode:** {}
+"""
