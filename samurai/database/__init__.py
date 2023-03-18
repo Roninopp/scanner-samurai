ec8d9b715bb17183a34fa4323c4f97e6a7204acc
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-03-18 15:01:29 +0530

    Update __init__.py

--- samurai/database/__init__.py
+++ samurai/database/__init__.py
@@ -11,8 +11,6 @@ db = client["samurai_scanner"]
 
 scan_db = db["scan_db"]
 token_db = db["token_db"]
-fban_db = db["fban_db"]
-misc_db = db["misc_db"]
 
 def get_gban_list():
     return [x for x in scan_db.find()]
