9bc5a31b5bc14c538ca547a2445fae70314a4a37
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-03-19 16:21:13 +0530

    Update help.py

--- samurai/plugins/help.py
+++ samurai/plugins/help.py
@@ -13,6 +13,10 @@ scan: **?scan <flag> <id> <reason> <bancode> <prooflink>**
 revert: **?revert <id>**
 fscan: **?fscan <id> <reason>**
 frevert: **?frevert <id>**
+id: Shows the user_id
+info: Shows the info about the user in the database
+token: Use it to get your token for the api
+
 sudolist: Shows the list of disasters
 scanlist: Shows the list of people who are scanned
 logs: Only for developers
