cfbb65b95afabf77d2acf828e1c7c81be992f08b
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-03-19 16:07:00 +0530

    Update scan_help.py

--- samurai/utils/scan_help.py
+++ samurai/utils/scan_help.py
@@ -25,3 +25,12 @@ def gban_save(user_id, target_name, reason, proof, bancode, enforcer):
 def revert_save(user_id):
     user_id = str(user_id)
     scan_db.delete_one({"user_id": user_id})
+
+
+def gban_data(user_id):
+    data = scan_db.find_one({"user_id": str(user_id)})
+    reason = data["reason"]
+    proof = data["proof"]
+    bancode = data["bancode"]
+    enforcer = data["enforcer"]
+    return reason, proof, bancode, enforcer
