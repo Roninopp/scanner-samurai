e21a8844b4207fdcaf87b497a5ac5c99c0a512a9
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-04-03 12:11:12 +0530

    Update scan.py

--- samurai/plugins/scan.py
+++ samurai/plugins/scan.py
@@ -1,4 +1,4 @@
-from samurai import pbot, tbot, SUDO_USERS, GBAN_CHANNEL_ID, SPAM_GROUP, SUPPORT_CHAT, SUPPORT_USERS
+from samurai import pbot, tbot, ubot, SUDO_USERS, GBAN_CHANNEL_ID, SPAM_GROUP, SUPPORT_CHAT, SUPPORT_USERS
 from samurai.utils.idform import createform
 
 from pyrogram import Client, enums, filters
@@ -8,9 +8,9 @@ from datetime import datetime
 import time
 from PIL import Image, ImageOps, ImageDraw
 
-from samurai.strings import scan_request_string, forced_scan_string, scan_approved_string, reject_string
+from samurai.strings import scan_request_string, forced_scan_string, scan_approved_string, reject_string, scan_string
 from samurai.utils.scan_help import check_gban, gban_save, revert_save
-from samurai.plugins.api import scan_api, revert_api
+#from samurai.plugins.api import scan_api, revert_api
 #from samurai.utils.bancode_help import bancode_convo
 
 
@@ -60,12 +60,48 @@ async def scan(_: Update, message: Message):
         await message.reply_text("this user is already scanned in our database!")
         return
 
+    user_idint = int(target_id)
+    case = user_idint//100000 + 1928
+    case_id = "#T" + str(case)
+
+    if flag == "F":
+        try:
+            target = await pbot.get_users(target_id)
+            target_name = target.first_name
+        except:
+            target_name = "Nodata"
+
+        if user_id not in SUDO_USERS:
+            return await message.reply_text("Only Enforcers can force me to scan!!!")
+
+        await message.reply_text("Connecting to Host team-samurai-X for a Force CYBER gban scan........")
+
+        gban_save(target_id, target_name, reason, proof, bancode, user_name)
+        await _.send_photo(
+            chat_id=GBAN_CHANNEL_ID,
+            photo="sample1.png",
+            caption=forced_scan_string.format(case_id, target_name, target_id, reason, proof, bancode, user_name),
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(text="Event", url=f"https://t.me/{SUPPORT_CHAT}/{message.id}")
+                    ]
+                ]
+            )
+        )
+        await message.reply_text(scan_approved_string.format("#LethalEliminator", target_name, target_id, reason, user_name))
+        await ubot.send_message(
+            chat_id=SPAM_GROUP,
+            text=f"/gban {target_id} {reason} Proof:{proof}"
+        )
+        return    
+            
     try:
         target = await pbot.get_users(target_id)
         target_name = target.first_name
         tuser_id = await tbot.get_entity(target_id)
     except Exception as e:
-        await message.reply_text("I dont know that user!!!\nPlease forward his/her message here and do ?info first")
+        await message.reply_text("I dont know that user!!!\nPlease forward his/her message here and do ?info first\nor\nuse flag -f to do a force scan with just user id")
         print(e)
         return
 
@@ -77,15 +113,13 @@ async def scan(_: Update, message: Message):
     except:
         result1 = createform(target_name, pfp=None)
 
-    user_idint = int(target_id)
-    case = user_idint//100000 + 1928
     crime_co = 200
 
     if flag == "-R":
         await _.send_photo(
             chat_id=GBAN_CHANNEL_ID,
             photo="user_form.png",
-            caption=scan_request_string.format(case_id, user_name, target_name, target_id, reason, proof, bancode),
+            caption=scan_request_string.format(case_id, target_name, target_id, reason, proof, bancode, user_name),
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
@@ -99,17 +133,17 @@ async def scan(_: Update, message: Message):
             )
         )
         await message.reply_text("Request Sent!!")
+        return
 
-    elif flag == "-F":
+    elif flag == "-G":
         if user_id not in SUDO_USERS:
-            return await message.reply_text("Only Enforcers can force me to scan!!!")
+            return await message.reply_text("Only Enforcers can make me scan!!!")
 
         gban_save(target_id, target_name, reason, proof, bancode, user_name)
-        ok = scan_api(target_id, target_name, reason, proof, bancode, user_name)
         await _.send_photo(
             chat_id=GBAN_CHANNEL_ID,
             photo="user_form.png",
-            caption=forced_scan_string.format(case_id, user_name, target_name, target_id, reason, proof, bancode),
+            caption=scan_string.format(case_id, target_name, target_id, reason, proof, bancode, user_name),
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
@@ -118,7 +152,11 @@ async def scan(_: Update, message: Message):
                 ]
             )
         )
-        await message.reply_text(scan_approved_string.format(target_name, target_id, reason, user_name))
+        await message.reply_text(scan_approved_string.format("#LethalEliminator", target_name, target_id, reason, user_name))
+        await ubot.send_message(
+            chat_id=SPAM_GROUP,
+            text=f"/gban {target_id} {reason} Proof:{proof}"
+        )
     else:
         await message.reply_text("Invalid Flag!!!")
 
@@ -133,9 +171,12 @@ async def about_commands_callbacc(_, CallbackQuery):
         target = await pbot.get_users(target_id)
         target_name = target.first_name
         gban_save(str(target_id), target_name, reason, proof, bancode, user_name)
-        ok = scan_api(str(target_id), target_name, reason, proof, bancode, user_name)
+        await ubot.send_message(
+            chat_id=SPAM_GROUP,
+            text=f"/gban {target_id} {reason} Proof:{proof}"
+        )
         await CallbackQuery.message.edit_caption(
-            scan_approved_string.format(target_name, target_id, reason, user_name),
+            scan_approved_string.format("#LethalEliminator", target_name, target_id, reason, user_name),
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
@@ -187,11 +228,9 @@ async def revert(_: Update, message: Message):
         return
 
     revert_save(target_id)
+    await ubot.send_message(
+        chat_id=SPAM_GROUP,
+        text=f"/ungban {target_id}"
+    )
     await pbot.send_message(SPAM_GROUP, f"USER [{target_id}](tg://openmessage?user_id={target_id}) UNSCANNED by {user_name}")
     await message.reply_text(f"Revert completed!!\n[User](tg://openmessage?user_id={target_id}) is Now free!!")
-
-    api_scan = revert_api(str(target_id))
-    if api_scan != "DONE":
-        return await message.reply_text(f"Failed!!! to revert")
-    else:
-        await message.reply_text(f"API Updated!!")
