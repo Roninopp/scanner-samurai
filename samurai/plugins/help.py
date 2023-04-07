84d5c122ce3cac76a31f539ddf4caa9f54ca5112
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-04-07 10:45:50 +0530

    Update help.py

--- samurai/plugins/help.py
+++ samurai/plugins/help.py
@@ -6,19 +6,81 @@ import time
 
 @Client.on_message(filters.command("help", prefixes="?"))
 async def start_all(_, message: Message):
-    textt = """
+    try:
+        tex = message.text.split(" ")[1]
+    except:
+        textt = """
+HELP: use like this-
+?help <command name>
+
+COMMANDS:
+```scan
+revert
+dev
+whois
+flags
+extra```
+"""
+        return await message.reply_text(textt)
+    if tex.upper() in ["SCAN"]:
+        textt = """
 Help:
 
 scan: **?scan <flag> <id> <reason> <bancode> <prooflink>**
-revert: **?revert <id>**
+eg: 
+```?scan -f 9829035992 spamming TSX01 https://proof```
+
 fscan: **?fscan <id> <reason>**
+eg:
+```?fscan 9829035992 spamming```
+
+for flags, see ?help flags
+"""
+    elif tex.upper() in ["REVERT"]:
+        textt = """
+Help:
+
+revert: **?revert <id>**
+eg:
+```?revert 9829035992```
+
 frevert: **?frevert <id>**
-id: Shows the user_id
-Whois: Shows the info about the user in the database
-token: Use it to get your token for the api
-Eval : ONLY INSPECTORS AND OWNER COMMAND
+eg:
+```?frevert 9829035992```
+"""
+    elif tex.upper() in ["DEV"]:
+        textt = """
+Help:
+
+Eval : ONLY DEVELOPERS AND OWNER COMMAND
 sudolist: Shows the list of disasters
 scanlist: Shows the list of people who are scanned
 logs: Only for developers
 """
+    elif tex.upper() in ["WHOIS"]:
+        textt = """
+Help:
+
+id: Shows the user_id
+Whois: Shows the info about the user in the database
+"""
+    elif tex.upper() in ["EXTRA"]:
+        textt = """
+Help:
+
+token: Use it to get your token for the api
+bancode: sends you the bancode
+report, appeal:
+sends a appeal to enforcers to scan that user
+"""
+    elif tex.upper() in ["FLAGS"]:
+        textt = """
+Help:
+
+-f : Force the bot to scan the user, even if he is not identified by the bot
+-r : Request a scan(For inspectors)
+-g : Noraml Scan
+"""
+    else:
+        textt = "Invalid!! help"
     await message.reply_text(textt)
