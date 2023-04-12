bfcb890bfd7a81d5650de565ad7ebb05b4ce1ee8
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-04-12 16:46:27 +0530

    Create sysinfo.py

---
+++ samurai/plugins/sysinfo.py
@@ -0,0 +1,49 @@
+from samurai import pbot, tbot, ubot, SUDO_USERS, GBAN_CHANNEL_ID, SPAM_GROUP, SUPPORT_CHAT, SUPPORT_USERS
+from pyrogram import Client, enums, filters
+from pyrogram.types import Message, Chat, User, InlineKeyboardMarkup, InlineKeyboardButton, Update
+import asyncio
+import time
+
+
+@ubot.on_message(filters.command("sysinfo", prefixes="?") & ~filters.private)
+async def ubscan(_: Update, message: Message):
+
+    user_id = message.from_user.id
+    if user_id in SUPPORT_USERS:
+        msg = await message.reply_text("Hold on have Give me a second Featching the Node Status")
+        asyncio.sleep(1)
+        await msg.edit_text("CONNECTING to Team samurai X scanner ■□□□□□")
+        asyncio.sleep(1)
+        await msg.edit_text("CONNECTING ■■□□□□")
+        asyncio.sleep(1)
+        await msg.edit_text("CONNECTING ■■■□□□")
+        asyncio.sleep(1)
+        await msg.edit_text("CONNECTING ■■■■□□")
+        asyncio.sleep(1)
+        await msg.edit_text("CONNECTING ■■■■■□")
+        asyncio.sleep(1)
+        await msg.edit_text("CONNECTION SUCESSFULL ■■■■■■")
+        asyncio.sleep(1)
+        await msg.edit_text("❇ 『YOU ARE VERYFIED USER』 ❇")
+        asyncio.sleep(2)
+        await msg.edit_text("*Version = v16.9.1\n\n*Codename = Gallium\n\n*Status = LTS\n\n*Total Core = 2")
+        asyncio.sleep(3)
+        await msg.edit_text("Welcome to Team Samurai system\n\n¤ 💎You Are Veryfied user💎\n\n Scan with proof.")
+    else:
+        msg = await message.reply_text("Hold on have Give me a second Featching the Node Status")
+        asyncio.sleep(1)
+        await msg.edit_text("CONNECTING to Team samurai X scanner ■□□□□□")
+        asyncio.sleep(1)
+        await msg.edit_text("CONNECTING ■■□□□□")
+        asyncio.sleep(1)
+        await msg.edit_text("CONNECTING ■■■□□□")
+        asyncio.sleep(1)
+        await msg.edit_text("CONNECTING ■■■■□□")
+        asyncio.sleep(1)
+        await msg.edit_text("CONNECTING ■■■■■□")
+        asyncio.sleep(1)
+        await msg.edit_text("CONNECTION SUCESSFULL ■■■■■■")
+        asyncio.sleep(1)
+        await msg.edit_text("❇ 『YOU ARE NOT A VERYFIED USER』 ❇")
+
+ 
